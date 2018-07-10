6686665c3fc759a0efe97bcda39c3066d196c35e
Srikanth Banagadi <amisrikanth@gmail.com>
2018-07-10 11:10:29 +0530

    finished this assignment

--- reverse-using-built-in-function.js
+++ reverse-using-built-in-function.js
@@ -1,5 +1,5 @@
 // The intent of this file is to reverse
 // the string using built in functions.
-const reverse = () => {};
+const reverse = input => input.split('').reverse().join('');
 
 module.exports = reverse;
--- reverse-using-iterators.js
+++ reverse-using-iterators.js
@@ -1,4 +1,15 @@
 // The intent of this file is to reverse the string using iterators (for, foreach)
-const reverse = () => {};
+const reverse = (input) => {
+  const str = [];
+  let i;
+  for (i = 0; i < Math.floor((input.length) / 2); i += 1) {
+    str[input.length - i - 1] = input[i];
+    str[i] = input[input.length - i - 1];
+  }
+  if (input.length % 2 === 1) {
+    str[i] = input[i];
+  }
+  return str.join('');
+};
 
 module.exports = reverse;
